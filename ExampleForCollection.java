bf9da0ec8986aa384f099bb9845313308ed19e20
pullururoja2004-hub <pullururoja2004@gmail.com>
2025-12-20 20:27:39 +0530

    Add files via upload

---
+++ ExampleForCollection.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class ExampleForCollection {
+    public static void main(String[] args){
+        List<Integer>l=new ArrayList<>();
+        List l1=new Vector();
+        List l2=new Stack();
+        List l3=new LinkedList();
+        List l4=new ArrayList();
+        l.add(10);
+        l.add(20);
+        l.add(30);
+        l.add(40);
+        System.out.println("List:"+l);
+        System.out.println("----------------------------------");
+        ArrayList al=new ArrayList();
+        al.add(10);
+        al.add(25);
+        al.add(30);
+        al.add(70);
+        System.out.println("ArrayList:"+al);
+        
+    }
+    
+}
