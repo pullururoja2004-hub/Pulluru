964d5329ad0435f43d55c4212371fe216d19f22c
pullururoja2004-hub <pullururoja2004@gmail.com>
2025-12-21 22:33:10 +0530

    Add files via upload

---
+++ ExampleForTreeSet.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ExampleForTreeSet {
+    public static void main(String[] args) {
+        TreeSet<Integer>ts=new TreeSet<Integer>();
+        ts.add(10);
+        ts.add(50);
+        ts.add(30);
+        ts.add(20);
+        for(Object i:ts){
+            System.out.println(i);
+        }
+    }
+}
