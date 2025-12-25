cb6f209f06e73ae9192a2bb49b9fe5bf40b469c0
pullururoja2004-hub <pullururoja2004@gmail.com>
2025-12-25 13:39:56 +0530

    Add files via upload

---
+++ ExampleForString.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ExampleForString {
+
+	public static void main(String[] args) {
+		/*String s=new String("Pulluru");
+		System.out.println(s);
+		
+		s.concat("Roja");
+		System.out.println(s);*/
+		
+		/*StringBuffer sb=new StringBuffer("Roja");
+		System.out.println(sb);
+		
+		sb.append("Pulluru");
+		System.out.println(sb);*/
+		StringBuilder sb=new StringBuilder("Roja");
+		System.out.println(sb);
+		
+		sb.append(" Pulluru");
+		System.out.println(sb);
+	}
+
+}
